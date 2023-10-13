d846eb9ce0ce9761d6e77a9637bda2be4fd8f8ee
Matt Seymour <matthewjamesseymour@gmail.com>
2023-10-14 00:17:21 +0100

    hidden fireabse config though not sure it matter

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,17 +2,7 @@
 	import { getFirestore ,updateDoc,doc, connectFirestoreEmulator} from 'firebase/firestore';
 	import { getAuth } from 'firebase/auth';
 	import { getStorage } from 'firebase/storage';
-
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCEiVIIF56wKk6fcQORuOeb6nRkP9jRrZk",
-    authDomain: "gotya-150d8.firebaseapp.com",
-    projectId: "gotya-150d8",
-    storageBucket: "gotya-150d8.appspot.com",
-    messagingSenderId: "759342163053",
-    appId: "1:759342163053:web:0e4a723d45c6c879758b64",
-    measurementId: "G-F5PDBSW6SH"
-  };
+  import {firebaseConfig} from '$lib/secrets/firebaseConfig';
 
 let firebaseApp
   if (!getApps.length){
@@ -26,7 +16,7 @@ let firebaseApp
 
   export const app = firebaseApp;
 	export const db = getFirestore(app);
-  connectFirestoreEmulator(db, '127.0.0.1', 8080);
+  //connectFirestoreEmulator(db, '127.0.0.1', 8080);
 	export const auth = getAuth(app);
 	export const storage = getStorage(app);
 
